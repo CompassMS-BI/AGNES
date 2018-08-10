2a94a2656fc2d879eda15b9882199b3c88094824
Brian Freeman <v-brif@microsoft.com>
2018-08-10 05:45:44 -0700

    Publishing

--- AGNES/My Project/AssemblyInfo.vb
+++ AGNES/My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("7.9.0.0")>
-<Assembly: AssemblyFileVersion("7.9.0.0")>
+<Assembly: AssemblyVersion("7.9.5.0")>
+<Assembly: AssemblyFileVersion("7.9.5.0")>
 <Assembly: NeutralResourcesLanguage("en-US")>
